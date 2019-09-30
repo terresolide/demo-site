83b1a27be4e2ab25b5439269bf7efb3981fd37c2
epointal <elisabeth.pointal@free.fr>
2019-09-30 13:42:54 +0200

    add in line interface

--- index.php
+++ index.php
@@ -22,8 +22,8 @@ include_once INCLUDE_DIR . "/language.php";
         "en" => "Towards a national modeling center « Earh System »"
     ),
     "presentation1" => array( 
-        "fr" => "Dans le cadre de la <a href='https://www.etalab.gouv.fr' target='_blank'>mission Etalab</a>, le <a href='https://www.poleterresolide.fr' target='_blank'>Pôle Terre Solide</a> développe une <a href='https://www.poleterresolide.fr/projets/en-cours/etalab/' target='_blank'>plateforme de traitement</a>",
-     		"en" => "As part of the <a href='https://www.etalab.gouv.fr' target='_blank'>Etalab mission</a>, the <a href='https://en.poleterresolide.fr' target='_blank'>Solid Earth Pole</a> is developping a <a href='https://www.poleterresolide.fr/projets/en-cours/etalab/' target='_blank'>treatment platform</a>"
+        "fr" => "Dans le cadre de la <a href='https://www.etalab.gouv.fr' target='_blank'>mission Etalab</a>, le <a href='https://www.poleterresolide.fr' target='_blank'>Pôle Terre Solide</a> a développé un prototype de <a href='https://www.poleterresolide.fr/projets/en-cours/etalab/' target='_blank'>plateforme de traitement à la demande</a>",
+     		"en" => "As part of the <a href='https://www.etalab.gouv.fr' target='_blank'>Etalab mission</a>, the <a href='https://en.poleterresolide.fr' target='_blank'>Solid Earth Pole</a> is developping a <a href='https://www.poleterresolide.fr/projets/en-cours/etalab/' target='_blank'>on demand treatment platform</a>"
  	)
  );
  $language = array_merge($language, $pagelanguage);
